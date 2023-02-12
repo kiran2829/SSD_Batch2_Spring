203e3f51400ad3c44422b54a8e6dc56e63fe6f8b
somarajukiran <kirangithub28@gmail.com>
2023-02-12 09:56:17 +0530

    Base Template

---
+++ SpringProject/src/main/java/com/ssd/spring/SpringProject/SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ssd.spring.SpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
---
+++ SpringProject/src/test/java/com/ssd/spring/SpringProject/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssd.spring.SpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
